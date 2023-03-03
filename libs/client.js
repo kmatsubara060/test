f78d7daa19b28e636d6bd81d6894c6b324ff5e7f
kmatsubara060 <k.matsubara@lincraft.co.jp>
2023-03-03 16:52:00 +0900

    first commit

---
+++ libs/client.js
@@ -0,0 +1,8 @@
+// libs/client.js
+ import { createClient } from 'microcms-js-sdk';
+
+ export const client = createClient({
+   serviceDomain: 'v99nw6wwjt',
+     apiKey:process.env.API_KEY,
+     });
+     
